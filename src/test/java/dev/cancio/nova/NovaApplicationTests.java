a757f15ea0965c2ae758e36ef35f2e8b85c06b9f
Filipe Câncio <filipe.cancio@outlook.com>
2020-06-07 11:11:05 -0300

    A new java application

---
+++ src/test/java/dev/cancio/nova/NovaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cancio.nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
